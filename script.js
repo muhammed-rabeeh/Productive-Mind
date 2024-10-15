2dadc8308dbc97630be1839c05f898425b1de35f
Muhammed Rabeeh K P <122214209+muhammed-rabeeh@users.noreply.github.com>
2024-10-15 12:17:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -351,7 +351,6 @@ function toggleStepTimer(taskIndex, stepIndex) {
     }
 }
 
-popup.style.display = "none";
 // Modify the startTimer function
 function startTimer() {
     clearInterval(targetTimer);
